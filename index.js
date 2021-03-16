3dc4e4f284a08a94bb29868838cedc238b5acb15
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-16 16:32:28 -0400

    commit tree 5397e38d16f5084521473aaf4e95f213f7b29023

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Write your code in this file!
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'; // free ride for under 400
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks."; // under 2500 cost $30
+  } else {
+    return "No can do."; // over 2500 = no ride
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; // 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
